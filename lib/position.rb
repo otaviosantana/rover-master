00e0367a5226f3c3a8b709fcb42eb07b0f45346e
Otavio Santana <otaviosantana@gmail.com>
2018-04-08 09:20:43 -0300

    Add class to represent position of rover on plateau

---
+++ lib/position.rb
@@ -0,0 +1,11 @@
+class Position
+
+  attr_accessor :x, :y, :direction
+
+  def initialize(position)
+    raise if position.nil? or position.length != 3
+    @x = position[0].to_i
+    @y = position[1].to_i
+    @direction = position[2]
+  end
+end
